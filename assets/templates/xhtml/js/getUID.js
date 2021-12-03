c868d4444d3857ae7169a5a00e16b1a1b7852ae9
kamil265 <m.insanulkamil26@gmail.com>
2021-12-03 08:52:09 +0700

    Update getUID.js

--- assets/templates/xhtml/js/getUID.js
+++ assets/templates/xhtml/js/getUID.js
@@ -10,14 +10,7 @@ function getUID(){
 }
 
 function show(data) {
-    $("#get_uidPas,#get_uidDok,#get_uidPerawat,#get_uidKar,#get_uidAset,#get_uidPj").html(data[0].rfid_uid);
-    // $("#get_uid").setAttribute('value',data[0].rfid_uid);
-
-    // document.getElementById('get_uid').html(data[0].rfid_uid);
-  }
-
-  function show(data) {
-    $("#get_uidPemindahanPasien").html(data[0].kode_rfid);
+    $("#get_uidPas,#get_uidDok,#get_uidPerawat,#get_uidKar,#get_uidAset,#get_uidPj,#get_uidPemindahanPasien").html(data[0].rfid_uid);
     // $("#get_uid").setAttribute('value',data[0].rfid_uid);
 
     // document.getElementById('get_uid').html(data[0].rfid_uid);
